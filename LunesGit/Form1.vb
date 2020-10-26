6ae1b57ae95b398870d971cc993183de1da954af
jon <jonbenja2@gmail.com>
2020-10-26 12:03:08 +0100

    comentario bonito

--- LunesGit/Form1.vb
+++ LunesGit/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("Buen inicio de semana ")
+        MsgBox("Buen inicio de semana ") 'este es un bonito comentario
     End Sub
 End Class
